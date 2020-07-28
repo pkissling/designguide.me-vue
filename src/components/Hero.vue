630218df5cecbe7c44994f5b4bd6907288d2d0d6
Patrick Kissling <patrick.kissling@gmx.de>
2020-07-28 12:14:56 +0200

    Add slide show for hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,17 +1,46 @@
 <template>
-  <div id="hero">
-    <div class="is-hidden-touch">
-      <img class="hero-img" loading="lazy" src="../assets/img/hero.png" />
-    </div>
-    <div class="is-hidden-desktop">
-      <img class="hero-img" loading="lazy" src="../assets/img/hero_small.png" />
-    </div>
+  <div id="hero" class="hero-img">
+
+    <!-- vue-concise-slider -->
+    <slider ref="slider" :options="options">
+      <slideritem>
+        <img class="hero-img" loading="lazy" :src='require("../assets/img/banner1.png")'/>
+      </slideritem>
+      <slideritem>
+        <img class="hero-img" loading="lazy" :src='require("../assets/img/banner2.png")'/>
+      </slideritem>
+      <slideritem>
+        <img class="hero-img" loading="lazy" :src='require("../assets/img/banner3.png")'/>
+      </slideritem>
+    </slider>
+
   </div>
 </template>
 
+<script>
+import { slider, slideritem } from "vue-concise-slider";
+export default {
+
+  components: {
+    slider,
+    slideritem,
+  },
+
+  data: () => {
+    return {
+      options: {
+        autoplay: 3000, // next after 3 sec
+        speed: 1000, // 1 sec transition
+        loop: true,
+        pagination: false
+      }
+    };
+  }
+};
+</script>
 <style scoped>
 .hero-img {
-  width: 100%
+  width: 100%;
 }
 
 #hero {
